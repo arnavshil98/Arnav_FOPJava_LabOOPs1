a58337cf910d9b67990b6600dfa2822dc0726fb8
arnavshil98 <arnv.shil@connaissant.com>
2022-09-19 23:51:20 +0530

    hrms portal made

---
+++ Arnav_FOPJava_LabOOPs1/src/com/fop/lab1/Employee.java
@@ -0,0 +1,80 @@
+package com.fop.lab1;
+
+import java.util.Random;
+
+public class Employee {
+
+	private String firstName;
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	private String lastName;
+	public Employee(String fname, String lname) {
+		this.firstName = fname;
+		this.lastName  = lname;
+	}
+	
+	public String dept(int i) throws Exception {
+		String dept = null;
+		switch (i) {
+		case 1:{
+			dept = "Technical";
+			break;
+		}
+		case 2:{
+			dept = "Admin";
+			break;
+		}
+		case 3:{
+			dept = "Human Resource";
+			break;
+		}
+		case 4:{
+			dept = "Legal";
+			break;
+		}
+		default:{
+			throw new Exception("department invalid");
+		}
+			
+		}
+		return dept;
+	}
+	
+	public StringBuffer systemPassword() {
+		StringBuffer prefix = new StringBuffer();
+		for (int i = 0; i < 3; i++) {
+			prefix = prefix.append(String.valueOf(Math.round(Math.random() * 10)));
+		}
+
+		String[] spChar = { "!", "@", "#", "$" };
+		int index = 0;
+		while (true) {
+			int indexTemp = (int) Math.round(Math.random() * 10);
+			if (indexTemp == 0 || indexTemp == 1 || indexTemp == 2 || indexTemp == 3) {
+				index = indexTemp;
+				break;
+			}
+
+		}
+		prefix.append(spChar[index]);
+		Random rd = new Random();
+		char smallLetter = (char) (rd.nextInt(26) + 'a');
+		prefix.append(smallLetter);
+
+		Random rd1 = new Random();
+		char capsLetter1 = (char) (rd1.nextInt(26) + 'A');
+		prefix.append(capsLetter1);
+
+		Random rd2 = new Random();
+		char capsLetter2 = (char) (rd2.nextInt(26) + 'A');
+		prefix.append(capsLetter2);
+
+		return prefix;
+	}
+}
---
+++ Arnav_FOPJava_LabOOPs1/src/com/fop/lab1/HrmsPortal.java
@@ -0,0 +1,42 @@
+package com.fop.lab1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class HrmsPortal {
+
+	public static void main(String[] args) throws Exception {
+
+		StringBuffer str = new StringBuffer();
+
+		Employee emp = new Employee("arnav", "shil");
+		str.append(emp.getFirstName());
+		String lstName = emp.getLastName();
+		str.append(lstName.concat("@"));
+		Scanner sc = new Scanner(System.in);
+		System.out.println("please select departments");
+		str.append(emp.dept(sc.nextInt()).concat(".greatlearning.com"));
+		
+		String fname = emp.getFirstName();
+		ArrayList<String> f1 = new ArrayList<>();
+		StringBuffer IntitCap = new StringBuffer();
+		for(int i = 0;i<fname.length();i++) {
+			if(i==0) {
+				f1.add(String.valueOf(fname.charAt(i)).toUpperCase());	
+			}
+			else{
+				f1.add(String.valueOf(fname.charAt(i)));
+			}
+			
+		}
+		for(String alphabet:f1) {
+			IntitCap.append(alphabet);
+		}
+		
+		System.out.println("Dear " + IntitCap +" your generated credentials are as follows");
+		System.out.println("Email --->"+ str);
+		
+		System.out.println("Password --->"+ emp.systemPassword());
+	}
+
+}
